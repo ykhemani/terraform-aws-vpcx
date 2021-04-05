8b08f7b452131323e324e542c50332d093f04db1
Yash Khemani <khemani@m13.home.seva.cafe>
2021-04-05 02:16:12 +0000

    reverse: enable nat gateway, enable dns hostnames

--- main.tf
+++ main.tf
@@ -21,9 +21,9 @@ module "vpc" {
   azs                  = ["${var.region}a", "${var.region}b", "${var.region}c"]
   private_subnets      = var.private_subnets
   public_subnets       = var.public_subnets
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  #enable_nat_gateway   = true
+  #single_nat_gateway   = true
+  #enable_dns_hostnames = true
 
   tags = {
     owner              = var.owner
